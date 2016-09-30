32753e9a94275fb787ecb193a24ae9ce9a60b14c
Jeferson Barros <soldier.jhonson@gmail.com>
2016-09-30 00:42:44 -0400

    First Commit

---
+++ app/src/main/java/com/example/android/trackconcurso/DisciplinasActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.trackconcurso;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DisciplinasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disciplinas);
+
+    }
+
+}
